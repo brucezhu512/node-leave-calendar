1aa293adc81b6a80534c60a029421d189bd9d665
Bruce.Zhu <brucezhu512@gmail.com>
2018-03-21 01:39:32 +0800

    Add copy-scripts.sh

---
+++ copy-scripts.sh
@@ -0,0 +1,9 @@
+cp -v ./node_modules/bootstrap/dist/js/bootstrap.min.* \
+      ./node_modules/jquery/dist/jquery.min.* \
+      ./node_modules/popper.js/dist/popper.min.* \
+      ./node_modules/feather-icons/dist/feather.min.* \
+      ./node_modules/chart.js/dist/Chart.min.* \
+      ./public/scripts/
+
+cp -v ./node_modules/bootstrap/dist/css/bootstrap.min.* \
+      ./public/stylesheets/
