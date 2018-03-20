42fe3346dfb7f395fad68fe7208a879e0323c35d
Bruce.Zhu <brucezhu512@gmail.com>
2018-03-21 01:49:02 +0800

    Create folder before copying files

--- copy-scripts.sh
+++ copy-scripts.sh
@@ -1,3 +1,4 @@
+mkdir -p ./public/scripts
 cp -v ./node_modules/bootstrap/dist/js/bootstrap.min.* \
       ./node_modules/jquery/dist/jquery.min.* \
       ./node_modules/popper.js/dist/popper.min.* \
@@ -5,5 +6,6 @@ cp -v ./node_modules/bootstrap/dist/js/bootstrap.min.* \
       ./node_modules/chart.js/dist/Chart.min.* \
       ./public/scripts/
 
+mkdir -p ./public/stylesheets
 cp -v ./node_modules/bootstrap/dist/css/bootstrap.min.* \
       ./public/stylesheets/
